6df7d36ad25f36553a90aec5ebfeb51a1fc7d96b
amrullah78 <amrullahelang@gmail.com>
2025-08-05 13:28:56 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2>CRM Kunjungan Salesman</h2>
+      <p>Selamat datang di aplikasi CRM.</p>
+    </div>
+  );
+};
+
+export default App;
